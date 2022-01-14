b4d49444090a1d252d77eeb9565ed517cdf0feda
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-13 23:48:32 -0500

    new value to return

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	value = line_tokenizator(&cmd, stack);
 	if (value != 0)
 	{
-		fprintf(stderr, "L<%d>: unknown instruction <opcode>\n", value);
+		fprintf(stderr, "L<%d>: unknown instruction <opcode>\n", value + 1);
 		exit(EXIT_FAILURE);
 	}
 	free(ptr);
